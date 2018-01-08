118a9750139eb970971a1fd0e2da18dd755ea677
Federico Balderas <federico.balderas@openpay.mx>
2018-01-08 10:11:55 -0600

    Corrección de texto

--- openpay-cards/templates/payment.php
+++ openpay-cards/templates/payment.php
@@ -27,11 +27,11 @@
 <div style="overflow: hidden;">
     <div style="overflow: hidden;">
         <div style="width: 35%; float: left;">
-            <h5>Trajetas de crédito</h5>	
+            <h5>Tarjetas de crédito</h5>	
             <img alt="" src="<?php echo $this->images_dir ?>credit_cards.png">	
         </div>
         <div style="width: 65%; float: left;">
-            <h5>Trajetas de débito</h5>	
+            <h5>Tarjetas de débito</h5>	
             <img alt="" src="<?php echo $this->images_dir ?>debit_cards.png">	
         </div>
     </div>	
